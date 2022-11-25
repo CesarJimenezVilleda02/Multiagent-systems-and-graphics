3415c71b666843aba0cde59a2d0278a43993d781
CesarJimenezVilleda02 <A01703517@tec.mx>
2022-11-25 10:56:30 -0600

    Gitignore fixed

--- Simulation/Assets/Scripts/SpawnManager.cs
+++ Simulation/Assets/Scripts/SpawnManager.cs
@@ -6,16 +6,38 @@ using System.Collections.Generic;
 public class SpawnManager : MonoBehaviour
 {
     public List<Vector3> lanes = new List<Vector3>(3);
+    // Zombie spawning
+    public GameObject zombie;
+    public float startZombieRangeLeft;
+    public float endZombieRangeLeft;
+    public float startZombieRangeRight;
+    public float endZombieRangeRight;
+    public float spawnPos;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("GenerateZombieLeft", startDelay, spawnInterval);
+        InvokeRepeating("GenerateZombieRight", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void GenerateZombieLeft()
+    {
+        Vector3 position = new Vector3(spawnPos, 0, Random.Range(startZombieRangeLeft, endZombieRangeLeft));
+        Instantiate(zombie, position, zombie.transform.rotation);
+    }
+    
+    void GenerateZombieRight()
+    {
+        Vector3 position = new Vector3(spawnPos, 0, Random.Range(startZombieRangeRight, endZombieRangeRight));
+        Instantiate(zombie, position, zombie.transform.rotation);
     }
 }
